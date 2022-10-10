820ae45f1918c6ce4207bb613b3f53de9b12ee46
balaxon <balahanov2209@gmail.com>
2022-10-10 16:10:00 +0300

    feat: add all js

--- assets/scripts/burger.js
+++ assets/scripts/burger.js
@@ -1,28 +1,40 @@
 const burger = document.querySelector('.burger');
 const navMenuMobile = document.querySelector('.logo_and_menu');
+const logoMobile = document.querySelector('.logo-mobile');
+const pageFader = document.querySelector('.page-fade');
+
+/*----------------------------------------burger-menu----------------------------------------*/
 
 function navSwitcher(el) {
 
     let target = el.target;
-    console.log(target);
     
     if (target === burger ) {
 
         burger.classList.toggle('burger-x')
         navMenuMobile.classList.toggle('burger-open');
+        logoMobile.classList.toggle('logo-fader');
+        pageFader.classList.toggle('displayed');
+        pageFader.classList.toggle('page-fader');
 
     } else if ((target.closest('.nav-item') || target.closest('.designed-text')) && !target.closest('.active-page')) {
 
         burger.classList.remove('burger-x')
         navMenuMobile.classList.remove('burger-open');
+        logoMobile.classList.remove('logo-fader');
+        pageFader.classList.remove('displayed', 'page-fader');
 
     } else if (target != navMenuMobile && !target.closest('.nav-and-designed') && !target.closest('.nav-wrapper')) {
 
         burger.classList.remove('burger-x')
         navMenuMobile.classList.remove('burger-open');
+        logoMobile.classList.remove('logo-fader');
+        pageFader.classList.remove('displayed', 'page-fader');
 
     }
 
 }
+
+/*----------------------------------------listeners----------------------------------------*/
 
 document.addEventListener('click', navSwitcher);
--- pages/donate/script.js
+++ pages/donate/script.js
@@ -4,6 +4,7 @@ const dollarsDesktop = document.querySelector('.range-dollars-desktop');
 const dollarsDesktopSmall = document.querySelector('.range-dollars-desktop-small');
 const dollarsTablet = document.querySelector('.range-dollars-tablet');
 let donateDollarsSum;
+let points;
 
 const rangeBg = document.querySelectorAll('.feed-days-range-background');
 const rangeDollars = document.querySelectorAll('.feed-days-range-dollars');
@@ -16,9 +17,44 @@ const rangeDollarsDesktopSmall = document.querySelector('.range-dollars-desktop-
 const window640 = window.matchMedia('(max-width: 980px)');
 const rangeBgTablet = document.querySelector('.range-background-tablet');
 const rangeDollarsTablet = document.querySelector('.range-dollars-tablet');
+const selfInput = document.querySelector('.self-sum-input');
+
+/*----------------------------------------length-check----------------------------------------*/
+
+function lengthCheck() {
+    if (selfInput.value.length > 4)
+    selfInput.value = selfInput.value.slice(0, 4)
+  }
+
+/*----------------------------------------self-sum-active----------------------------------------*/
+
+function setSelfSumActive(range, self, sums, dots) {
+
+    for (let item of sums) {
+        item.classList.remove('active-summ');
+    }
+
+    for (let dot of dots) {
+        dot.classList.remove('range-point-active');
+    }
+
+    for (let i = 0; i < sums.length; i++) {
+        if (self.value === sums[i].lastElementChild.textContent) {
+
+            range.value = i;
+            range.classList.add('x');
+            sums[i].classList.add('active-summ');
+            dots[i].classList.add('range-point-active');
+        }
+    }
+}
+
+/*----------------------------------------media----------------------------------------*/
 
 function media () {
     if (window1600.matches) {
+        points = rangeBgDesktop.getElementsByTagName('li');
+
         for (let item of rangeBg) {
             item.classList.add('range-disabled');
         }
@@ -30,22 +66,34 @@ function media () {
         rangeBgDesktop.classList.remove('range-disabled');
         rangeDollarsDesktop.classList.remove('range-disabled');
         donateRangeSum.setAttribute('max', '7');
-        donateRangeSum.setAttribute('value', '2');
+        donateRangeSum.setAttribute('value', '5');
 
         donateDollarsSum = dollarsDesktop.querySelectorAll('.feed-days-range-donate-summ ');
 
         function setDonateSumActive() {
+
             for (let item of donateDollarsSum) {
-                item.classList.remove('active-summ')
+                item.classList.remove('active-summ');
             }
 
-            donateDollarsSum[donateRangeSum.value].classList.add('active-summ')
+            for (let point of points) {
+                point.classList.remove('range-point-active');
+            }
+
+            points[donateRangeSum.value].classList.add('range-point-active');
+            donateDollarsSum[donateRangeSum.value].classList.add('active-summ');
+            selfInput.value = donateDollarsSum[donateRangeSum.value].lastElementChild.textContent;
+
         }
 
         donateRangeSum.addEventListener('change', setDonateSumActive);
+        donateRangeSum.addEventListener('click', setDonateSumActive);
+        selfInput.addEventListener('input', () => setSelfSumActive(donateRangeSum, selfInput, donateDollarsSum, points));
         setDonateSumActive();
 
     } else if (window1280.matches && !window640.matches) {
+        points = rangeBgDesktopSmall.getElementsByTagName('li');
+
         for (let item of rangeBg) {
             item.classList.add('range-disabled');
         }
@@ -57,7 +105,7 @@ function media () {
         rangeBgDesktopSmall.classList.remove('range-disabled');
         rangeDollarsDesktopSmall.classList.remove('range-disabled');
         donateRangeSum.setAttribute('max', '6');
-        donateRangeSum.setAttribute('value', '1');
+        donateRangeSum.setAttribute('value', '4');
 
         donateDollarsSum = dollarsDesktopSmall.querySelectorAll('.feed-days-range-donate-summ ');
 
@@ -66,13 +114,23 @@ function media () {
                 item.classList.remove('active-summ')
             }
 
-            donateDollarsSum[donateRangeSum.value].classList.add('active-summ')
+            for (let point of points) {
+                point.classList.remove('range-point-active');
+            }
+
+            points[donateRangeSum.value].classList.add('range-point-active');
+            donateDollarsSum[donateRangeSum.value].classList.add('active-summ');
+            selfInput.value = donateDollarsSum[donateRangeSum.value].lastElementChild.textContent;
         }
 
         donateRangeSum.addEventListener('change', setDonateSumActive);
+        donateRangeSum.addEventListener('click', setDonateSumActive);
+        selfInput.addEventListener('input', () => setSelfSumActive(donateRangeSum, selfInput, donateDollarsSum, points));
         setDonateSumActive();
 
     } else if (window640.matches) {
+        points = rangeBgTablet.getElementsByTagName('li');
+
         for (let item of rangeBg) {
             item.classList.add('range-disabled');
         }
@@ -93,13 +151,24 @@ function media () {
                 item.classList.remove('active-summ')
             }
 
-            donateDollarsSum[donateRangeSum.value].classList.add('active-summ')
+            for (let point of points) {
+                point.classList.remove('range-point-active');
+            }
+
+            points[donateRangeSum.value].classList.add('range-point-active');
+            donateDollarsSum[donateRangeSum.value].classList.add('active-summ');
+            selfInput.value = donateDollarsSum[donateRangeSum.value].lastElementChild.textContent;
         }
 
         donateRangeSum.addEventListener('change', setDonateSumActive);
+        donateRangeSum.addEventListener('click', setDonateSumActive);
+        selfInput.addEventListener('input', () => setSelfSumActive(donateRangeSum, selfInput, donateDollarsSum, points));
         setDonateSumActive();
     }
 }
 
+/*----------------------------------------listeners----------------------------------------*/
+
 window.addEventListener('resize', media);
 media();
+selfInput.addEventListener('input', lengthCheck);
